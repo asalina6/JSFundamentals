6da3b18fcf0ca3ea90cf3a10d1dba40432f4bbf2
Armando Salinas <armandosalinasiiiarizona@gmail.com>
2020-03-31 06:15:10 -0700

    Ah it does do comment only files. I just forgot to add this folder.

---
+++ JavascriptDefinitiveGuide/Chapter 2/ch2.js
@@ -0,0 +1,21 @@
+/*Outline
+ ch 2 - Lexical structure
+ 2.1 Character set 
+    2.1.1 case sensitivity
+    2.1.2 whitespace, line breaks, format control characters
+    2.1.3 Unicode Escape Sequenecs
+    2.1.4 Normalization
+2.2 Comments
+2.3 Literals
+2.4 Identifiers and Reserved Words
+    2.4.1 REserved Words
+2.5 Optional Semicolons
+*/
+
+
+
+/*Author goes over lexical structure. While there are interesting examples of not using
+semicolon (which leads to some errors), the point is that we should use semi-colons
+(like Douglas Crockford recommends us to do).
+*/
+
