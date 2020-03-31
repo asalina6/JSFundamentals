0be1acadbb20df3aeb1e65c9ce3df4623a3a3108
Armando Salinas <armandosalinasiiiarizona@gmail.com>
2020-03-31 06:14:03 -0700

    Learned that git does not just track comment only files

---
+++ JavascriptDefinitiveGuide/Chapter 1/ch1.js
@@ -0,0 +1,10 @@
+/* Ch 1 - Introduction to Javascript
+    1.1 Core Javascript
+    1.2 Client-side Javascript
+*/
+
+
+/* Nothing of interest here. Just a quick overview of Javascript. Author shows examples of concepts he will go over in the book.
+He does make the distinction of CORE vs Client-side JS. The first half of the book is core*/
+
+var x = 1; //Getting git to save this.
